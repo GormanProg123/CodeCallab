f4ed41af233d8356907f4cbbe84e9f5ff1f76a80
GormanProg123 <gparfenchik2005@gmail.com>
2025-02-22 14:05:06 +0100

    Setup Redux Toolkit with Persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from './store/store'
+import { increment, decrement } from './store/slices/exampleSlice'
+
 export const App = () => {
-  return <></>;
-};
+  const count = useSelector((state: RootState) => state.example.count)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h1>Redux Toolkit + Persist</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  )
+}
